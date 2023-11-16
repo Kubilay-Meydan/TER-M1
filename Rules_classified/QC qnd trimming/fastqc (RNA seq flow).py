c646eb171c3eeda9cc5301860cecef52e7a2496d
Kubilay <kubilay.meydan2002@gmail.com>
2023-11-16 11:57:30 +0100

    started work on a comparator GUI, extracted 15 modules for QC and trimming.

---
+++ Rules_classified/QC qnd trimming/fastqc (RNA seq flow).py
@@ -0,0 +1,11 @@
+rule fastqc:
+   input:
+      f1 = config['datadirs']['fastq'] + "/" + "{file}_{read}.fq.gz"
+   output: config['datadirs']['qc'] + "/" + "{file}_{read}_fastqc.html", config['datadirs']['qc'] + "/" + "{file}_{read}_fastqc.zip"
+   params:
+      prefix =  config['datadirs']['qc'],
+   resources:
+      mem_mb= 10000
+   shell:
+        """
+        fastqc  --thread 8 --outdir {params.prefix} --nogroup {input.f1}
