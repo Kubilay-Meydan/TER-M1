c848aaa1b1fb3f11ad4e1231f440ddd58d99dcf2
Kubilay <kubilay.meydan2002@gmail.com>
2023-12-14 11:31:38 +0100

    attributes rule name

--- rules_for_comparison/snakemake-example-master/fastqc.py
+++
@@ -1,5 +0,0 @@
-rule fastqc:
-	input: "raw/{sample}.trimmed.fastq.gz"
-	output: "fastqc/{sample}.trimmed_fastqc.zip"
-	shell: "{TOOLDIR}/FastQC/fastqc -o fastqc {input}"
-	
--- rules_for_comparison/snakemake-example-master/sortbam.py
+++
@@ -1,6 +0,0 @@
-#novosort can index
-rule sortbam:
-	input: "{sample}.bam"
-	output: bam="{sample}.sorted.bam", bai="{sample}.sorted.bam.bai"
-	threads: 24
-	shell: "{SORT} -t {TMPDIR} -s -i -o {output.bam} {input}"
